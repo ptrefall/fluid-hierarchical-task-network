d11232212a703af8e3dfd26d7386dbf67bc7162e
Pål Trefall <pal.trefall@sonat.no>
2019-04-15 15:29:13 +0200

    Started on domain tests.

---
+++ Fluid-HTN.UnitTests/DomainTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FluidHTN;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fluid_HTN.UnitTests
+{
+    [TestClass]
+    public class DomainTests
+    {
+        [TestMethod]
+        public void DomainHasRootWithDomainName_ExpectedBehavior()
+        {
+            var domain = new Domain<MyContext>("Test");
+            Assert.IsTrue(domain.Root != null);
+            Assert.IsTrue(domain.Root.Name == "Test");
+        }
+    }
+}
