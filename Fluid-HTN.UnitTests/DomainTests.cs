5343dce494d7414fb0897f63a40aacf088a1eb65
Pål Trefall <ptrefall@facepunchstudios.com>
2019-04-16 09:07:36 +0200

    More work on domain unit tests.

--- Fluid-HTN.UnitTests/DomainTests.cs
+++ Fluid-HTN.UnitTests/DomainTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using FluidHTN;
+using FluidHTN.Compounds;
+using FluidHTN.PrimitiveTasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Fluid_HTN.UnitTests
@@ -13,6 +15,38 @@ namespace Fluid_HTN.UnitTests
             var domain = new Domain<MyContext>("Test");
             Assert.IsTrue(domain.Root != null);
             Assert.IsTrue(domain.Root.Name == "Test");
+        }
+
+        [TestMethod]
+        public void AddSubtaskToParent_ExpectedBehavior()
+        {
+            var domain = new Domain<MyContext>("Test");
+            var task1 = new Selector() { Name = "Test" };
+            var task2 = new PrimitiveTask() { Name = "Test" };
+            domain.Add(task1, task2);
+            Assert.IsTrue(task1.Subtasks.Contains(task2));
+            Assert.IsTrue(task2.Parent == task1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException), AllowDerivedTypes = false)]
+        public void FindPlanMustInitContext_ExpectedBehavior()
+        {
+            var ctx = new MyContext();
+            var domain = new Domain<MyContext>("Test");
+            var plan = domain.FindPlan(ctx);
+            Assert.IsTrue(plan != null);
+            Assert.IsTrue(plan.Count == 0);
+        }
+
+        [TestMethod]
+        public void FindPlanNoTasksThenNoPlan_ExpectedBehavior()
+        {
+            var ctx = new MyContext();
+            ctx.Init();
+            var domain = new Domain<MyContext>("Test");
+            var plan = domain.FindPlan(ctx);
+            Assert.IsTrue(plan == null);
         }
     }
 }
